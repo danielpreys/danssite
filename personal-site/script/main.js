c099027d73fb7e64e9ba45b7dc085612ab3eb249
danielpreys <danielpreys@yahoo.com>
2018-03-21 22:05:47 -0700

    added resume

--- personal-site/script/main.js
+++ personal-site/script/main.js
@@ -1,5 +1,12 @@
-
-$(document).ready(function(){
-    
-
+   
+/*
+$(".arrow").click(function(){
+    $(".expands").animate({height: "300px"});
+}); 
+ 
+$(".arrow").hover(function(){
+    $(this).css("color", "#E6E6FA ");
+},
+function(){
+    $(this).css("color", "black");
 });
