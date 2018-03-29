b6a31db5a854e776f5d48df0f48c4a31226846ce
danielpreys <danielpreys@yahoo.com>
2018-03-28 21:58:21 -0700

    removed resume, and contact section

--- personal-site/script/main.js
+++ personal-site/script/main.js
@@ -1,12 +1,2 @@
    
-/*
-$(".arrow").click(function(){
-    $(".expands").animate({height: "300px"});
-}); 
- 
-$(".arrow").hover(function(){
-    $(this).css("color", "#E6E6FA ");
-},
-function(){
-    $(this).css("color", "black");
-});
+
